28e58f74dad8bdd8440e2b0b15931268e1a2b607
Sigmar Þór Hávarðarson <sigmarthor@gmail.com>
2015-05-09 13:46:19 +0000

    Wrong DbContext

--- hringr/Repository/GroupRepository.cs
+++ hringr/Repository/GroupRepository.cs
@@ -1,11 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
+using hringr.Models;
 
 namespace hringr.Repository
 {
     public class GroupRepository
     {
+        ApplicationDbContext m_db = new ApplicationDbContext();
+
+        public Group GetGroupById(int id)
+        {
+            var result = (from n in m_db.Groups
+                          where n.ID == id
+                          select n).SingleOrDefault();
+            return result;
+        }
+
+        public void CreateGroup(Group n)
+        {
+            m_db.Groups.Add(n);
+            m_db.SaveChanges();
+        }
+
+        public void DeleteGroup(Group n)
+        {
+            m_db.Groups.Remove(n);
+            m_db.SaveChanges();
+        }
     }
 }
