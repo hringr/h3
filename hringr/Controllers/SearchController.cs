4a744bfd818acbd1dfff0a985ca2ec9e7e7d9e62
Sigmar Þór Hávarðarson <sigmarthor@gmail.com>
2015-05-09 16:55:57 +0000

    Search

---
+++ hringr/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using hringr.Models;
+
+namespace hringr.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+        //
+        // GET: /Search/
+        public ActionResult Index(string q)
+        {
+            var users = (from u in db.Users
+                         where u.UserName.Contains(q)
+                         select u);
+
+            return View(users);
+        }
+	}
+}
