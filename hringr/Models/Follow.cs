90f6dff658b8985d8649ef8f9f8fcb49700f30b0
Halldora Johannsdottir <hj02@ru.is>
2015-05-11 15:18:55 +0000

    virtual user added in Follows

--- hringr/Models/Follow.cs
+++ hringr/Models/Follow.cs
@@ -7,5 +7,7 @@ namespace hringr.Models
         public int ID { get; set; }
         public int followerID { get; set; }
         public int followeeID { get; set; }
+
+        public virtual ApplicationUser user { get; set; }
     }
 }
