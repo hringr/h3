8733546192999ebe0e246126e8491522d53d22de
Sigmar Þór Hávarðarson <sigmarthor@gmail.com>
2015-05-12 13:04:58 +0000

    SearchRepo to handle db queries

---
+++ hringr/Repository/SearchRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using hringr.Models;
+
+namespace hringr.Repository
+{
+    public class SearchRepository
+    {
+        private static SearchRepository _instance;
+        public static SearchRepository Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new SearchRepository();
+                return _instance;
+            }
+        }
+
+        static readonly ApplicationDbContext m_db = new ApplicationDbContext();
+
+        //public IEnumerable<ApplicationUser> SearchForUsers(string str)
+        //{
+        //    var results = from x in m_db.Users
+        //                  where x.
+        //} 
+	}
+}
