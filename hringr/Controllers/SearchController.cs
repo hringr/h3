f0b6003838bef3193d76a6d153d5414fc8804380
SvanaJona <svanhildure13@ru.is>
2015-05-12 16:24:24 +0000

    meira search

--- hringr/Controllers/SearchController.cs
+++ hringr/Controllers/SearchController.cs
@@ -12,7 +12,8 @@ namespace hringr.Controllers
         public ActionResult Index(string q)
         {
             var users = (from u in db.Users
-                         where u.FullName.Contains(q)
+                         where (u.FullName.Contains(q))
+                         || (u.UserName.Contains(q))
                          select u);
 
             return View(users);
